cd8e8438cde6a70cf29e886c2098f557dfac5af5
Adeniji Oluwaferanmi <itech@trado.ng>
2021-04-01 07:28:28 +0100

    initial push

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {LandingPage} from './pages/LandingPage';
 import FxRate from './pages/fx-rate';
 import { BrowserRouter,Route} from 'react-router-dom';
+import EasterPromo from './pages/easterpromo';
+
 function App() {
   return (
      
@@ -9,7 +11,7 @@ function App() {
       
         <Route exact path="/" component={LandingPage}/>
         <Route path="/fx-rates-pic" component={FxRate}/>
-      
+        <Route path="/easter2k" component={EasterPromo} />
     </BrowserRouter>
   );
 }
