76d655aeac667d6c1cd2feb3dd712dc177c8c273
Adeniji Oluwaferanmi <itech@trado.ng>
2021-04-05 10:00:32 +0100

    fixed robot

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import {LandingPage} from './pages/LandingPage';
 import FxRate from './pages/fx-rate';
-import { BrowserRouter,Route} from 'react-router-dom';
+import { BrowserRouter,Switch,Route} from 'react-router-dom';
 import EasterPromo from './pages/easterpromo';
 
 function App() {
   return (
      
      <BrowserRouter>
-      
+      <Switch>
         <Route exact path="/" component={LandingPage}/>
         <Route path="/fx-rates-pic" component={FxRate}/>
         <Route path="/easter2k" component={EasterPromo} />
+        <Route component={LandingPage} />
+        </Switch>
     </BrowserRouter>
   );
 }
